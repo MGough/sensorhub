40b0c9cf94ca86c5ec000f6fb6367f3ee3d7fa31
Merlin Gough <6558104+MGough@users.noreply.github.com>
2020-02-03 00:27:15 +0000

    document public methods

--- sensorhub/hub.py
+++ sensorhub/hub.py
@@ -59,6 +59,11 @@ class SensorHub:
         return False
 
     def get_off_board_temperature(self) -> int:
+        """
+        Returns the temperature in degrees celcius.
+        :return: Temperature in degrees celcius, or -1 if it's out of range
+        :raises IOError: Raised if the sensor is not connected
+        """
         if self._is_off_board_temperature_out_of_range():
             return -1
         return self._read_sensor_board_register(SensorRegister.OFF_BOARD_TEMPERATURE)
@@ -66,21 +71,33 @@ class SensorHub:
     def _is_temperature_and_humidity_data_up_to_date(self) -> bool:
         """
         Unlike other errors, this often happens so we handle it more gracefully than throwin an exception
-        :return:
+        :return: boolean representing whether or not the data is up to date
         """
         return self._read_sensor_board_register(SensorRegister.ON_BOARD_SENSOR_OUT_OF_DATE) == 0
 
     def get_humidity(self) -> int:
+        """
+        Get the humidity sensor reading
+        :return: Humidity as a percentage, or -1 if the data is not up to date
+        """
         if self._is_temperature_and_humidity_data_up_to_date():
             return self._read_sensor_board_register(SensorRegister.ON_BOARD_HUMIDITY)
         return -1
 
     def get_temperature(self) -> int:
+        """
+        Get the on board temperature
+        :return: Temperature in celcius, or -1 if the data is not up to date
+        """
         if self._is_temperature_and_humidity_data_up_to_date():
             return self._read_sensor_board_register(SensorRegister.ON_BOARD_TEMPERATURE)
         return -1
 
     def is_motion_detected(self) -> bool:
+        """
+        Find out if any motion was detected recently
+        :return: true if motion was recently detected
+        """
         return self._read_sensor_board_register(SensorRegister.MOTION) == 1
 
     def _is_brightness_out_of_range(self) -> bool:
@@ -93,6 +110,11 @@ class SensorHub:
         return False
 
     def get_brightness(self) -> int:
+        """
+        get brightness sensor reading in Lux
+        :return: brightness in Lux, -1 if the reading is out of range
+        :raises IOError: raised if there's a hardware issue with the light sensor (i.e. shoudn't happen)
+        """
         if self._is_brightness_out_of_range():
             return -1
         else:
@@ -107,14 +129,24 @@ class SensorHub:
         raise IOError("Barometric Sensor Error")
 
     def get_barometer_temperature(self) -> int:
+        """
+        get barometer temperature sensor reading in celcius
+        :return: temperature in celcius
+        :raises IOError: raised if there's a hardware issue with the barometer (i.e. shouldn't happen)
+        """
         if self._is_barometer_working():
             return self._read_sensor_board_register(SensorRegister.BAROMETRIC_TEMPERATURE)
 
     def get_barometer_pressure(self) -> float:
+        """
+        get barometer pressure sensor reading in celcius
+        :return: pressure in hPa (hectopascals)
+        :raises IOError: raised if there's a hardware issue with the barometer (i.e. shouldn't happen)
+        """
         if self._is_barometer_working():
             pascals = self._read_sensor_board_register(SensorRegister.BAROMETRIC_PRESSURE_LOW) \
                       | self._read_sensor_board_register(SensorRegister.BAROMETRIC_PRESSURE_MIDDLE) << 8 \
                       | self._read_sensor_board_register(SensorRegister.BAROMETRIC_PRESSURE_HIGH) << 16
 
-            # convert to hPa (hectopascals)
+            # convert to hPa
             return round(pascals * 0.01, ndigits=2)
